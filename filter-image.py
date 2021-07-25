36d1469102c6fe01a19733c54c27b915119f5215
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-07-26 01:38:49 +0400

    update

--- filter-image.py
+++ filter-image.py
@@ -1,26 +1,54 @@
-import PIL
+import numpy as np 
+import cv2 as cv
+from PIL import Image, ImageOps
+import math
+from numba import njit, prange
+import os
 import streamlit as st
-import numpy as np
-from PIL import Image, ImageFilter
+from skimage.morphology import erosion, dilation, opening, closing
+from skimage.morphology import skeletonize
+from skimage.morphology import square
 
 def getImage(fileupload):
     try:
-        img = Image.open(fileupload)
+        temp = Image.open(fileupload)
+        temp = np.array(ImageOps.grayscale(temp))
+        return temp
     except:
-        print("Что-то пошло не так...")
-    return img
+        st.error("Что-то пошло не так")
 
-def clearImage(img: Image.Image)->Image.Image:
-    filtered_img = img.filter(ImageFilter.MedianFilter(size=int(st.sidebar.slider("Выберете степень фильтрации", min_value=3, max_value=9, step=2))))
-    return filtered_img
+def genNoisy(img, lvl):
+    noise = np.ones_like(img) * lvl * (img.max() - img.min())
+    noise[np.random.random(size=noise.shape) > 0.5] *= -1
+    return noise
+    
+
+def threshold_processing(img: np.ndarray, threshold: np.uint8)->np.ndarray:
+    output = np.where(img > threshold, 255, 0)
+    return output
 
 def main():
-    fileupload = st.file_uploader("Upload your photo, support *.png, *.jpg, *.jpeg formats",)
+    fileupload = st.file_uploader("Загрузите фотографию",)
     if fileupload is not None:
         img = getImage(fileupload)
-        st.image(img, caption="Ваше изображение")
-        filtered_img = clearImage(img)
-        st.image(filtered_img, caption="Изображение после фильтрации")    
+        cols1, cols2 = st.beta_columns(2)
+        with cols1:
+            st.image(img, caption="Ваше изображение")
+        with cols2:
+            lvl = st.sidebar.select_slider("Размер элемента",options=np.arange(1,10,1))
+            selem = square(lvl)
+            func = st.sidebar.selectbox("Функция",["Эрозия","Дилатация","Открытие","Закрытие", "Скелетонизация"])
+            if func in "Эрозия":
+                output = erosion(img, selem)
+            elif func in "Дилатация":
+                output = dilation(img, selem)
+            elif func in "Открытие":
+                output = opening(img, selem)
+            elif func in "Закрытие":
+                output = closing(img, selem)
+
+            st.image(output, caption="{}".format(func))
+            pass   
 
 if __name__ == "__main__":
     main()
--- oleg-report.py
+++ oleg-report.py
@@ -7,31 +7,41 @@ import plotly.express as px
 import altair as alt
 
 def main():
-    st.title("Интерактивный отчет")
-    df = pd.read_csv(os.path.join("data_oleg_report", "dataset.csv"), parse_dates=True, index_col="Date")
-    cols = st.sidebar.multiselect("Выберите столбцы для визуализации", options=df.columns[1:])
-    visual = st.sidebar.selectbox("Выберите библиотеку визуализации", ["Vega", "Plotly"])
-    unique_years = df.index.year.unique()
-    years = st.sidebar.select_slider("Выберите год для визуализации",options = unique_years)
-    years_end = st.sidebar.select_slider("Выберите год окончания для визуализация",options = unique_years)
-    df_chart = df.loc[str(years):str(years_end)]
-    if st.sidebar.button("Привести все значения к диапазону [0;1]"):
-        df_chart = (df_chart - df_chart.min()) / (df_chart.max() - df_chart.min())
-    if visual in "Vega":
-        st.write(f"График за все года до {years}")
-        st.line_chart(df_chart[cols])
-    elif visual in "Plotly":
-        st.plotly_chart(
-            px.line(data_frame=df_chart, y=cols)
-        )
-    with st.beta_expander("Статистика"):   
-        st.markdown("Общая статистика")
-        stats = df_chart[cols].describe()
-        st.dataframe(stats)
-        st.markdown("Матрица кореляции Пирсона")
-        corr_matrix = df_chart[cols].corr()
-        st.dataframe(corr_matrix)
-    
+    try:
+        st.title("Интерактивный отчет")
+        df = pd.read_csv(os.path.join("data_oleg_report", "dataset.csv"), parse_dates=True, index_col="Date")
+        cols = st.sidebar.multiselect("Выберите столбцы для визуализации", options=df.columns[1:])
+        visual = st.sidebar.selectbox("Выберите библиотеку визуализации", ["Vega", "Plotly"])
+        unique_years = df.index.year.unique()
+        st.sidebar.info("Выберете диапазон используя слайдеры")
+        years = st.sidebar.select_slider("Выберите год начала для визуализации",options = unique_years)
+        years_end = st.sidebar.select_slider("Выберите год окончания для визуализация",options = unique_years)
+        df_chart = df.loc[str(years):str(years_end)]
+        if st.sidebar.checkbox("Привести все значения к диапазону [0;1]"):
+            df_chart = (df_chart - df_chart.min()) / (df_chart.max() - df_chart.min())
+        if visual in "Vega":
+            st.write(f"График за период {years}-{years_end}")
+            st.line_chart(df_chart[cols])
+            # st.write(f"Гистограмма за период {years}-{years_end}")
+            # st.bar_chart(df_chart[cols])
+        elif visual in "Plotly":
+            st.write(f"График за период {years}-{years_end}")
+            st.plotly_chart(
+                px.line(data_frame=df_chart, y=cols)
+            )
+            # st.write(f"Гистограмма за период {years}-{years_end}")
+            # st.plotly_chart(
+            #     px.histogram(data_frame=df_chart, y=cols)
+            # )
+        with st.beta_expander("Статистика"):   
+            st.markdown("Общая статистика")
+            stats = df_chart[cols].describe()
+            st.dataframe(stats)
+            st.markdown("Матрица кореляции Пирсона")
+            corr_matrix = df_chart[cols].corr()
+            st.dataframe(corr_matrix)
+    except:
+        st.warning("Выберите какую-нибудь из колонок")
 
     
 
