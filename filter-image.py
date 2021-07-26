3e42f9d7591cde7b08a311d4e5b1c83f9ad15291
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-07-26 20:17:09 +0400

    update

--- filter-image.py
+++ filter-image.py
@@ -1,13 +1,13 @@
 import numpy as np 
-import cv2 as cv
 from PIL import Image, ImageOps
 import math
-from numba import njit, prange
+import matplotlib.pyplot as plt
 import os
 import streamlit as st
 from skimage.morphology import erosion, dilation, opening, closing
 from skimage.morphology import skeletonize
 from skimage.morphology import square
+from skimage.util import invert
 
 def getImage(fileupload):
     try:
@@ -22,10 +22,17 @@ def genNoisy(img, lvl):
     noise[np.random.random(size=noise.shape) > 0.5] *= -1
     return noise
     
+def getHistFigure(img:np.ndarray):
+    histogram, axs = plt.subplots()
+    axs = plt.hist(img.ravel(), bins=255,color="gray")
+    # axs.xlabel("Значение серого")
+    # axs.ylabel("Количество пикселей")
+    return histogram
 
-def threshold_processing(img: np.ndarray, threshold: np.uint8)->np.ndarray:
-    output = np.where(img > threshold, 255, 0)
-    return output
+
+# def threshold_processing(img: np.ndarray, threshold: np.uint8)->np.ndarray:
+#     output = np.where(img > threshold, 255, 0)
+#     return output
 
 def main():
     fileupload = st.file_uploader("Загрузите фотографию",)
@@ -34,10 +41,12 @@ def main():
         cols1, cols2 = st.beta_columns(2)
         with cols1:
             st.image(img, caption="Ваше изображение")
+            st.markdown("Гистограмма изображения")
+            st.pyplot(getHistFigure(img))
         with cols2:
             lvl = st.sidebar.select_slider("Размер элемента",options=np.arange(1,10,1))
             selem = square(lvl)
-            func = st.sidebar.selectbox("Функция",["Эрозия","Дилатация","Открытие","Закрытие", "Скелетонизация"])
+            func = st.sidebar.selectbox("Функция",["Эрозия","Дилатация","Открытие","Закрытие"])
             if func in "Эрозия":
                 output = erosion(img, selem)
             elif func in "Дилатация":
@@ -48,6 +57,8 @@ def main():
                 output = closing(img, selem)
 
             st.image(output, caption="{}".format(func))
+            st.markdown("Гистограмма изображения")
+            st.pyplot(getHistFigure(output))
             pass   
 
 if __name__ == "__main__":
